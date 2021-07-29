59fb4a0d439ec75b78544f9e7a75ea1c15cc9223
code salley <juxsalley@gmail.com>
2021-07-29 11:30:08 +0000

    validation and test case for photos, user -> many albums -> many photos max of 10

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Album, type: :model do
-  describe "Album modal associations" do
+  describe "Album modal associations, user can have have many albums but each album will have max photos of 10" do
     it { should have_many(:photos).class_name(:Photo).dependent(:destroy) }
     it { should validate_length_of(:photos) }
     it { should belong_to(:user).class_name("User") }
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Photo model associations, belongs to both the user and album, this will enable us perform CRUD on images individually " do
+    it { should belong_to(:album).validate(true) }
+    it { should belong_to(:user).class_name(:User).with_foreign_key(:user_id) }
+  end
 end
